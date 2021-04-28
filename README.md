ce15c27450c9e2979a94e7ef7721f5afa921a7d1
Gustavo Andrade <gustavogyn@gmail.com>
2021-04-27 22:17:54 -0300

    Commit inicial

---
+++ README.md
@@ -0,0 +1,14 @@
+
+Para rodar, siga os seguintes passos
+
+1. Clone este repositório
+
+2. Entre na pasta do projeto
+
+3. `docker network create pfa-desafio1`
+
+4. `docker run --name mysql -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=fullcycle -p 3306:3306 --network pfa-desafio1 -v $(pwd)/mysqldata:/var/lib/mysql -d mysql:5.7`
+
+5. `docker run --name desafio1 -e MYSQL_HOST=mysql -e MYSQL_PORT=3306 -p 3000:3000 --network pfa-desafio1 -d gustavoar/pfa-desafio1:1.0`
+
+6. `docker run --name mynginx1 -p 8080:80 -v $(pwd)/nginx.conf:/etc/nginx/nginx.conf:ro --network pfa-desafio1 -d nginx`
---
+++ main.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
+	"net/http"
+	"os"
+)
+
+var port string
+var DB *sql.DB
+
+type Course struct {
+	Id    int    `json:"id"`
+	Title string `json:"title"`
+}
+
+func AllCourses() ([]Course, error) {
+	if DB == nil {
+		return nil, nil
+	}
+
+	rows, err := DB.Query("SELECT * FROM FULL_CYCLE")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var courses []Course
+
+	for rows.Next() {
+		var course Course
+
+		err := rows.Scan(&course.Id, &course.Title)
+		if err != nil {
+			return nil, err
+		}
+
+		courses = append(courses, course)
+	}
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return courses, nil
+}
+
+func getAllCourses(w http.ResponseWriter, r *http.Request) {
+	courses, err := AllCourses()
+	if err != nil {
+		fmt.Fprintf(w, "Error while obtaining courses")
+	}
+	json.NewEncoder(w).Encode(courses)
+}
+
+func handleRequests() {
+	http.HandleFunc("/", getAllCourses)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+}
+
+func fillDatabase() error {
+	_, err := DB.Exec("CREATE TABLE IF NOT EXISTS FULL_CYCLE (id INT AUTO_INCREMENT PRIMARY KEY, title VARCHAR(255) NOT NULL)")
+	if err != nil {
+		return err
+	}
+	var exists bool
+	row := DB.QueryRow("SELECT EXISTS (SELECT 1 FROM FULL_CYCLE)")
+	if err := row.Scan(&exists); err != nil {
+		return err
+	}
+
+	if !exists {
+		if _, err := DB.Exec("INSERT INTO FULL_CYCLE (title) VALUES('DOCKER'),('Fundamentos de Arquitetura de Software'),('Comunicação'),('RabbitMQ'),('Autenticação e Keycloak'),('Domain Driven Design e Arquitetura hexagonal')"); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func main() {
+	var err error
+
+	mysqlHost := os.Getenv("MYSQL_HOST")
+	if mysqlHost == "" {
+		mysqlHost = "127.0.0.1"
+	}
+	mysqlPort := os.Getenv("MYSQL_PORT")
+	if mysqlPort == "" {
+		mysqlPort = "3306"
+	}
+	port = os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+
+	DB, err = sql.Open("mysql", fmt.Sprintf("root:root@tcp(%s:%s)/fullcycle", mysqlHost, mysqlPort))
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer DB.Close()
+
+	err = fillDatabase()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	handleRequests()
+}
